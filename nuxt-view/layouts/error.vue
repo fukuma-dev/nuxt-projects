8faf35f0265bdded574001b16fe0a3dab5d58ba8
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-08-24 17:06:32 +0900

    init

---
+++ nuxt-view/layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 v-if="error.statusCode === 404">Error: 404 Not Foundだよ</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
